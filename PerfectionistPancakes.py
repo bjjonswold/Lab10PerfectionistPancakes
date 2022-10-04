03db333f39c884a20b983ea30bdb80bc13d43c35
Emilie Beck <98420041+themilkprince@users.noreply.github.com>
2022-10-04 10:19:56 -0600

    Update PerfectionistPancakes.py

--- PerfectionistPancakes.py
+++ PerfectionistPancakes.py
@@ -27,7 +27,7 @@ def orderUp(totalNum, orders):
 #What is this function doing?
 def main():
     pancakeStack = [takeOrders()]
-    while(input("Would you like anything else? y/n\n").lower() != “n”):
+    while(input("Would you like anything else? y/n\n").lower() != "n"):
 	pancakeStack.append(takeOrders())
     madeOrder = onePlateToAnother(pancakeStack)
     print("Your order of {} is up!".format(madeOrder))
