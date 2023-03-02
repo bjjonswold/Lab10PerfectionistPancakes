784b4eaaa051ecd7394c115b37d358d28d3c22c7
bjjonswold <bjjonswold@gmail.com>
2023-03-02 14:02:45 -0700

    Try 1

--- PerfectionistPancakes.py
+++ PerfectionistPancakes.py
@@ -1,3 +1,11 @@
+'''
+1. Flow diagram for onePlatetoAnother() - See Paper
+2. The output for this would be ['choco chip', 'strawberry', 'blueberry']
+3. The base case for this is if the length of pancakes is 0, then the return will be an empty list
+4. The output for main in this case would be Your order of ['birthday cake', 'strawberry', 'maple'] is up!
+'''
+
+
 #input: user input
 #output: user input
 def takeOrders():
@@ -16,19 +24,26 @@ def separateOrders(str):
 #output: you answer :)
 def onePlateToAnother(pancakes):
     if(len(pancakes) == 0):
-	return []
+	    return []
     else:
-	return [pancakes[len(pancakes)-1]] + onePlateToAnother(pancakes[:len(pancakes) - 1])
+	    return [pancakes[len(pancakes)-1]] + onePlateToAnother(pancakes[:len(pancakes) - 1])
+
+print('TESTING:', onePlateToAnother(['blueberry', 'strawberry', 'choco chip']))
+order = []
 
 def orderUp(totalNum, orders): 
     #your code here!
+    if totalNum == 0:
+        return order
+    order[totalNum - 1] = separateOrders(orders[totalNum - 1])
+
     return
 
 #What is this function doing?
 def main():
     pancakeStack = [takeOrders()]
     while(input("Would you like anything else? y/n\n").lower() != "n"):
-	pancakeStack.append(takeOrders())
+	    pancakeStack.append(takeOrders())
     madeOrder = onePlateToAnother(pancakeStack)
     print("Your order of {} is up!".format(madeOrder))
 
